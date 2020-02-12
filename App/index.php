af8d648815cc417c31c1a620f863422061a1a161
Alexander Korneev <korneev@tutu.ru>
2020-02-12 20:13:19 +0300

    try connect to db

--- App/index.php
+++ App/index.php
@@ -2,3 +2,19 @@
 require_once('../vendor/autoload.php');
 
 echo 'тут живет бот, иди куда шел';
+
+
+$dbopts = parse_url(getenv('DATABASE_URL'));
+
+$driver = 'pgsql';
+$user = $dbopts["user"];
+$pass = $dbopts["pass"];
+$host = $dbopts["host"];
+$port = $dbopts["port"];
+$db = ltrim($dbopts["path"], '/');
+try {
+    $conn = new PDO("pgsql:host=$host dbname=$db", $user, $pass);
+    echo 'АТАС';
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
