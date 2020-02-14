7795b87a287401646c676dacf4b244248953056d
Alexandra Ponomareva <ponomareva@tutu.ru>
2020-02-14 12:36:31 +0300

    add scale for decimal columns and create all tables in 1 migration

--- migrations/20200213185935_create_tables.php
+++ migrations/20200213185935_create_tables.php
@@ -6,9 +6,14 @@ class CreateTables extends AbstractMigration
 {
     public function up()
     {
+        $sessionTable = $this->table('session');
+        $sessionTable->addColumn('is_active', 'boolean', ['default' => 1])
+            ->addColumn('chat_id', 'string')
+            ->addIndex(['chat_id']);
+        $sessionTable->save();
 
         $paymentTable = $this->table('payment');
-        $paymentTable->addColumn('sum', 'decimal', ['null' => false])
+        $paymentTable->addColumn('sum', 'decimal', ['scale' => 2, 'null' => false])
             ->addColumn('session_id', 'integer')
             ->addColumn('description', 'string', ['default' => ''])
             ->addColumn('is_open', 'boolean', ['default' => 1])
@@ -18,9 +23,9 @@ class CreateTables extends AbstractMigration
         $sessionTable = $this->table('debt');
         $sessionTable->addColumn('payment_id', 'integer')
             ->addColumn('description', 'string', ['default' => ''])
-            ->addColumn('amount', 'decimal', ['null' => false])
+            ->addColumn('amount', 'decimal', ['scale' => 2, 'null' => false])
             ->addColumn('user_debtor', 'string', ['null' => false])
-            ->addColumn('creditor', 'string', ['null' => false])
+            ->addColumn('user_creditor', 'string', ['null' => false])
             ->addColumn('is_open', 'boolean', ['default' => 1])
             ->addIndex(['payment_id']);
         $sessionTable->save();
@@ -30,5 +35,6 @@ class CreateTables extends AbstractMigration
     {
         $this->table('debt')->drop()->save();
         $this->table('payment')->drop()->save();
+        $this->table('session')->drop()->save();
     }
 }
