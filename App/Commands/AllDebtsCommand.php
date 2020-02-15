0e104d79b5277e826ffa6a52ada2f156a217fd82
Alexandra Ponomareva <ponomareva@tutu.ru>
2020-02-15 22:20:39 +0300

    temp change

--- App/Commands/AllDebtsCommand.php
+++ App/Commands/AllDebtsCommand.php
@@ -17,19 +17,71 @@ class AllDebtsCommand  extends UserCommand
     protected $usage = '/all_debts';                                           // Usage of your command
     protected $version = '1.0.0';                                             // Version of your command
 
+//    public function execute()
+//    {
+//        $message = $this->getMessage();            // Get Message object
+//
+//        $chatId = $message->getChat()->getId();   // Get the current Chat ID
+//
+//        try {
+//            $sessionId = (new SessionTable())->getLastActiveSessionByChatId($chatId);
+//        } catch (\Throwable $e) {
+//            $sessionId = 1; // TODO как надо обработать ошибку?
+//        }
+//
+//        $text = $this->prepareRawDebtsText($sessionId);
+//
+//        $data = [                                  // Set up the new message data
+//            'chat_id' => $chatId,                  // Set Chat ID to send the message to
+//            'text'    => $text                 // Set message to send
+//        ];
+//
+//
+//        return Request::sendMessage($data);        // Send message!
+//    }
+//
+//    /**
+//     *  Все долги по текущей сессии без аггрегации, но с комментариями
+//     */
+//    private function prepareRawDebtsText(int $session)
+//    {
+//        $hiText = "Эй, юзеры! \n";
+//        $debtText = '';
+//        $debtsData = $this->getDebtTable()->getAllActiveDebts($session);
+//        foreach ($debtsData as $debt) {
+//            if (isset($debt['user_debtor']) && $debt['user_creditor'] && $debt['amount'] && $debt['description']) {
+//                $debtText .= "{$debt['user_debtor']} должен {$debt['user_creditor']} {$debt['amount']} за \"{$debt['description']}\".\n";
+//            }
+//        }
+//        if (empty($debtText)) {
+//            $debtText =  "Поздравляю! У вас нет долгов в текущей сессии";
+//        }
+//        return $hiText . $debtText;
+//    }
+//
+//    private function getDebtTable()
+//    {
+//        return new DebtTable();
+//    }
+
+
     public function execute()
     {
         $message = $this->getMessage();            // Get Message object
 
         $chatId = $message->getChat()->getId();   // Get the current Chat ID
 
+        $text = "Эй, юзеры! \n";
         try {
             $sessionId = (new SessionTable())->getLastActiveSessionByChatId($chatId);
         } catch (\Throwable $e) {
             $sessionId = 1; // TODO как надо обработать ошибку?
         }
+        $debtsData = $this->getRowDebts($sessionId);
 
-        $text = $this->prepareRawDebtsText($sessionId);
+        foreach ($debtsData as $debt) {
+            $text .= "{$debt['user_debtor']} должен {$debt['user_creditor']} {$debt['amount']} за \"{$debt['description']}\".\n";
+        }
 
         $data = [                                  // Set up the new message data
             'chat_id' => $chatId,                  // Set Chat ID to send the message to
@@ -41,22 +93,11 @@ class AllDebtsCommand  extends UserCommand
     }
 
     /**
-     *  Все долги по текущей сессии без аггрегации, но с комментариями
+     *  Простой подсчет - сколько Петя должен Васе без учета того, сколько они оба должны Саше
      */
-    private function prepareRawDebtsText(int $session)
+    private function getRowDebts(int $session)
     {
-        $hiText = "Эй, юзеры! \n";
-        $debtText = '';
-        $debtsData = $this->getDebtTable()->getAllActiveDebts($session);
-        foreach ($debtsData as $debt) {
-            if (isset($debt['user_debtor']) && $debt['user_creditor'] && $debt['amount'] && $debt['description']) {
-                $debtText .= "{$debt['user_debtor']} должен {$debt['user_creditor']} {$debt['amount']} за \"{$debt['description']}\".\n";
-            }
-        }
-        if (empty($debtText)) {
-            $debtText =  "Поздравляю! У вас нет долгов в текущей сессии";
-        }
-        return $hiText . $debtText;
+        return $this->getDebtTable()->getAllActiveDebts($session);
     }
 
     private function getDebtTable()
